fe20b73e86f88ffde4a35795aaabb96646279b6c
Henrik Fricke <henrikfricke@web.de>
2020-07-03 19:13:01 +0200

    fix(semantic): use release.config.js file

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  release: {
+    branch: 'master',
+    verifyConditions: [
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/git',
+      '@qiwi/semantic-release-gh-pages-plugin',
+    ],
+    publish: [
+      '@semantic-release/npm',
+      '@semantic-release/github',
+      [
+        '@qiwi/semantic-release-gh-pages-plugin',
+        {
+          src: 'dist-storybook',
+        },
+      ],
+    ],
+  },
+};
