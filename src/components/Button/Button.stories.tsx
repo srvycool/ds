408454a0cd11ff7cce52e0c14c0f3cf1b86385a1
Henrik Fricke <henrikfricke@web.de>
2020-07-03 16:52:20 +0200

    chore: basic SC/TS setup

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import { Button } from './Button';
 
 export default {
   title: 'Button',
@@ -9,12 +9,4 @@ export default {
 
 export const Text: React.FC = () => (
   <Button onClick={action('clicked')}>Hello Button</Button>
-);
-
-export const Emoji: React.FC = () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
 );
