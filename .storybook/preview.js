06779c0110365187a75871b87f2638e73d1e26a7
Henrik Fricke <henrikfricke@web.de>
2020-07-05 20:29:33 +0200

    fix: load font in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,7 @@ import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { ThemeProvider } from '../src';
 
+import 'typeface-hammersmith-one';
+import 'typeface-montserrat';
+
 addDecorator(storyFn => <ThemeProvider>{storyFn()}</ThemeProvider>);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import 'typeface-hammersmith-one';
-import 'typeface-montserrat';
-
 export * from './components/Button/Button';
 export * from './components/ThemeProvider/ThemeProvider';
