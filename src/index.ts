1ee1a86de970359fd493d0eb75e291129017f393
Henrik Fricke <henrikfricke@web.de>
2020-07-07 23:41:53 +0200

    fix: export components in index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+export * from './components/Box/Box';
 export * from './components/Button/Button';
+export * from './components/Input/Input';
+export * from './components/Logo/Logo';
+export * from './components/Textarea/Textarea';
 export * from './components/ThemeProvider/ThemeProvider';
 export * from './components/Typography/Typography';
